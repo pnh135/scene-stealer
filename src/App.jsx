069129b99d64a10bfc54f6bac4c4f9d0590eda1d
Koi-0 <dndud5182@gmail.com>
2025-02-13 19:32:31 +0900

    Feat : GlobalStyle 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Router from './shared/Router';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <>
+      {/* GlobalStyle로 기본적인 페이지 레이아웃 적용 */}
+      <GlobalStyle />
       <Router />
     </>
   );
