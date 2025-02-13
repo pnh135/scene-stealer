1db8e17eb40fdf135981d6e8c34e6feadcc188ed
Koi-0 <dndud5182@gmail.com>
2025-02-13 19:30:10 +0900

    Style : GlobalStyle 수정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,11 +1,17 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset};
 
     body {    
-        
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        display: flex;
+        place-items: center;
+        min-width: 320px;
+        min-height: 100vh;
     }
 `;
 
