feda824c433398804f77b4994553f526dbc514e4
kwakhana <you@example.com>
2025-02-13 20:07:48 +0900

    Docs: Login, Signup, Sidebar, Detail 컴포넌트 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Sidebar from "../pages/sidebar";
+import Detail from "../pages/Detail";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/sidebar" element={<Sidebar/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
       </Routes>
     </BrowserRouter>
   );
