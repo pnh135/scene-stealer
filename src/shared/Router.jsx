bf7fef002a130882fc2137a1c7d79499f96b3114
JungminLee97 <jungmin7618@naver.com>
2025-02-13 19:40:50 +0900

    Stlye: 사이드바 뼈대 대충충 완성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Side from '../pages/Side';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Side />} />
       </Routes>
     </BrowserRouter>
   );
