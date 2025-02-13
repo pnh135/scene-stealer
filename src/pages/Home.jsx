8691ed0cc75e0c1685388d1aee17c2d4491efcf3
Koi-0 <dndud5182@gmail.com>
2025-02-13 18:18:49 +0900

    Move : 파일 이름 수정 [Home.jsx -> HomePage.jsx]

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <>
-      <div>Home-page 영역입니다.</div>
-    </>
-  );
-};
-
-export default Home;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <div>로고</div>
+        <div>검색창</div>
+        <div>
+          <div>로그인</div>
+          <div>회원가입</div>
+        </div>
+      </header>
+      <aside>
+        <div>피드 추가</div>
+        <div>북마크</div>
+        <div>알림</div>
+        <div>설정</div>
+      </aside>
+      <footer>푸터</footer>
+    </div>
+  );
+};
+
+// const homeWarrper
+
+export default HomePage;
