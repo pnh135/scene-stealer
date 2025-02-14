b6149b346561b4812b14a9725b8c38d818bd2c16
JDY <wjsehdud@nate.com>
2025-02-14 16:57:24 +0900

    style: 공용 뒤로가기 버튼 스타일링 완성성

--- src/public-button/PublicButton.jsx
+++ src/public-button/PublicButton.jsx
@@ -9,7 +9,11 @@ const PublicButton = () => {
 };
 
 const PublicCommonStyle = styled.button`
-  padding: 10px 15px;
+  align-items: center;
+  word-wrap: break-word;
+  padding: 10px 10px;
+  font-size: 0.8rem;
+  font-weight: bold;
   border: none;
   border-radius: 1rem;
   background-color: rgb(0, 0, 114);
@@ -18,6 +22,27 @@ const PublicCommonStyle = styled.button`
   &:hover {
     transition: background-color 0.4s ease;
     background-color: rgb(180, 240, 80);
+  }
+`;
+
+const PublicBackButtonStyle = styled.button`
+  align-items: center;
+  word-wrap: break-word;
+  padding: 10px 10px;
+  font-size: 0.8rem;
+  font-weight: bold;
+  border: none;
+  border-radius: 1rem;
+  background-color: #242424;
+  color: wheat;
+  cursor: pointer;
+  &:hover {
+    transition: background-color 0.4s ease;
+    background-color: rgb(114, 0, 0);
+  }
+  &:active {
+    background-color: #ff1515; /* 클릭했을 때 배경색 변경 */
+    transform: scale(0.98); /* 클릭 시 버튼이 살짝 작아지는 효과 */
   }
 `;
 
