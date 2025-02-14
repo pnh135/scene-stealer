7c520b7f7d455c1d0a695d3a033b51786c115273
Koi-0 <dndud5182@gmail.com>
2025-02-15 01:23:11 +0900

    Rename	: 대소문자 구별 후 파일 이름 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import MainPage from '../pages/MainPage';
 import AddFeedPage from '../pages/AddFeedPage';
-import BookMarksPage from '../pages/BookMarksPage';
+import BookMarkPage from '../pages/BookMarkPage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import DetailPage from '../pages/DetailPage';
@@ -14,7 +14,7 @@ const Router = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/main/add-feed" element={<AddFeedPage />} />
-        <Route path="/main/bookmarks" element={<BookMarksPage />} />
+        <Route path="/main/bookmark" element={<BookMarkPage />} />
         <Route path="/main/login" element={<LoginPage />} />
         <Route path="/main/signup" element={<SignupPage />} />
         <Route path="/main/detail/:id" element={<DetailPage />} />
