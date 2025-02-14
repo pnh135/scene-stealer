553af52d44fed4bf5ea0114841f90bd6503f2733
Koi-0 <dndud5182@gmail.com>
2025-02-14 17:05:22 +0900

    Style : 불필요한 스타일 제거

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -17,21 +17,10 @@ const SideBar = () => {
 };
 
 const SideBarWrapper = styled.div`
-  background-color: #d6dad5;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  width: 200px;
-  height: 1200px;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top: -10px;
-  margin-left: -10px;
+  background-color: #fff8e9;
 `;
 
 const SideBarButton = styled.button`
-  width: 100px;
-  height: 100px;
   background-color: #e90d0df7;
   justify-content: center;
   align-items: center;
