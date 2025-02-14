803408591fbc1e07c9916817c7abb4dbfb00cfde
kwakhana <you@example.com>
2025-02-14 13:37:14 +0900

    Docs: Siderbar 렌더링 위치 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Sidebar from "../pages/sidebar";
+import Sidebar from "../pages/Sidebar";
 import Detail from "../pages/Detail";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Sidebar/> //전체 페이지 내 사이드 바 렌더링
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
-        <Route path="/sidebar" element={<Sidebar/>} />
         <Route path="/detail/:id" element={<Detail/>} />
       </Routes>
     </BrowserRouter>
