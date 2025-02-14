130c6bf22d526714012eebaecfaab7f463706e69
Koi-0 <dndud5182@gmail.com>
2025-02-14 16:24:40 +0900

    Move : HomePage의 기능을 MainPage로 옮김

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-// import styled from 'styled-components';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -7,54 +6,9 @@ const HomePage = () => {
   return (
     <>
       홈페이지 영역입니다.
-      <button onClick={() => navigate('/main')}>버튼</button>
-      {/* <HomePageContainer>
-        <HomePageWrapper>
-          <HomePageHeader>header</HomePageHeader>
-          <HomePageContent>
-            <HomePageAside>side</HomePageAside>
-            <HomePageMain>main</HomePageMain>
-          </HomePageContent>
-          <HomePageFooter>footer</HomePageFooter>
-        </HomePageWrapper>
-      </HomePageContainer> */}
+      <button onClick={() => navigate('/main')}>메인페이지로 이동하기</button>
     </>
   );
 };
-
-// const HomePageContainer = styled.main`
-//   margin: 0 auto;
-//   width: 100%;
-//   text-align: center;
-// `;
-
-// const HomePageWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const HomePageHeader = styled.header`
-//   background-color: lightcoral;
-// `;
-
-// const HomePageContent = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   background-color: lightblue;
-// `;
-
-// const HomePageAside = styled.aside`
-//   background-color: yellowgreen;
-//   flex: 1;
-// `;
-
-// const HomePageMain = styled.div`
-//   background-color: orange;
-//   flex: 10;
-// `;
-
-// const HomePageFooter = styled.footer`
-//   background-color: gray;
-// `;
 
 export default HomePage;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -6,9 +7,54 @@ const MainPage = () => {
   return (
     <>
       메인 페이지 영역입니다.
-      <button onClick={() => navigate('/')}>버튼</button>
+      <button onClick={() => navigate('/')}>홈페이지로 이동하기</button>
+      <HomePageContainer>
+        <HomePageWrapper>
+          <HomePageHeader>header</HomePageHeader>
+          <HomePageContent>
+            <HomePageAside>side</HomePageAside>
+            <HomePageMain>main</HomePageMain>
+          </HomePageContent>
+          <HomePageFooter>footer</HomePageFooter>
+        </HomePageWrapper>
+      </HomePageContainer>
     </>
   );
 };
+
+const HomePageContainer = styled.main`
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+`;
+
+const HomePageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const HomePageHeader = styled.header`
+  background-color: lightcoral;
+`;
+
+const HomePageContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: lightblue;
+`;
+
+const HomePageAside = styled.aside`
+  background-color: yellowgreen;
+  flex: 1;
+`;
+
+const HomePageMain = styled.div`
+  background-color: orange;
+  flex: 10;
+`;
+
+const HomePageFooter = styled.footer`
+  background-color: gray;
+`;
 
 export default MainPage;
