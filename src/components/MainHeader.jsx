40a2ef428b807501f0e74a7e74ba522ca97cf3c3
Koi-0 <dndud5182@gmail.com>
2025-02-14 17:49:34 +0900

    Chore : MainHeader styled-components 추가

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+const MainHeader = () => {
+  return (
+    <MainPageHeader>
+      <HeaderLogo>로고</HeaderLogo>
+      <HeaderSearch>검색창</HeaderSearch>
+      <HeaderLogin>
+        <HeaderLoginSignIn>로그인</HeaderLoginSignIn>
+        <HeaderLoginSignUp>회원가입</HeaderLoginSignUp>
+      </HeaderLogin>
+    </MainPageHeader>
+  );
+};
+
+const MainPageHeader = styled.header`
+  background-color: lightcoral;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+  padding: 10px;
+`;
+
+const HeaderLogo = styled.div`
+  background-color: red;
+  flex: 1.5;
+`;
+
+const HeaderSearch = styled.div`
+  background-color: lightcyan;
+  flex: 6;
+`;
+
+const HeaderLogin = styled.div`
+  background-color: lightgoldenrodyellow;
+  flex: 1.5;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const HeaderLoginSignIn = styled.div`
+  background-color: lightgrey;
+`;
+
+const HeaderLoginSignUp = styled.div`
+  background-color: lightgreen;
+`;
+
+export default MainHeader;
