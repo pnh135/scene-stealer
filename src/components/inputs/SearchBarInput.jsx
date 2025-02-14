54b85256c8f7fb243a44b04c7cf3f252afbfcb32
hj <hyeonjubaek06@gmail.com>
2025-02-15 06:18:59 +0900

    Feat: SearchBarInput 컴포넌트에 검색 기능 추가

--- src/components/inputs/SearchBarInput.jsx
+++ src/components/inputs/SearchBarInput.jsx
@@ -1,7 +1,24 @@
 
 const SearchBarInput = () => {
+    const [search, setSearch] = useState('');
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+    };
+
     return (
-        <div>SearchBarInput</div>
+        <div>
+            <form action="">
+                <input
+                    type="text"
+                    name="search"
+                    value={search}
+                    onChange={handleSearchChange}
+                    placeholder="검색어를 입력하세요"
+
+                />
+            </form>
+        </div>
     )
 }
 
