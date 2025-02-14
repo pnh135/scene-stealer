9d38571ee19f34228ddaa38d8069b84cd0ab84f7
JungminLee97 <jungmin7618@naver.com>
2025-02-14 23:08:43 +0900

    Feat: 저장된로컬리지를 불러오기위한로직구현과 이의 필요한 usestate, useEffect훅 사용과 map을이용한 렌더링

--- src/pages/BookmarksPage.jsx
+++ src/pages/BookmarksPage.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+import MOCK_DATA from './MOCK_DATA';
+
 const BookmarksPage = () => {
-  return <div>북마크페이지입니다</div>;
+  const [bookmarks, setBookmarks] = useState([]);
+  useEffect(() => {
+    const savedBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    setBookmarks(savedBookmarks);
+  }, []);
+  return (
+    <div>
+      북마크페이지입니다
+      {bookmarks.length > 0 ? (
+        bookmarks.map((id) => {
+          const item = MOCK_DATA.find((li) => li.id === id);
+          return item ? (
+            <div key={id}>
+              <img src={item.img_url} alt={item.korean_name} />
+              <div>{item.korean_name}</div>
+              <div>{item.description}</div>
+            </div>
+          ) : null;
+        })
+      ) : (
+        <p>북마크된 아이템이 없습니다.</p>
+      )}
+    </div>
+  );
 };
 
 export default BookmarksPage;
