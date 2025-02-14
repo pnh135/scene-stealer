8df33d775beeb0d0ba287ea541255d60b332f0e9
kwakhana <you@example.com>
2025-02-14 14:39:03 +0900

    Docs: Sidebar 삭제

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,13 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Sidebar from "../pages/Sidebar";
 import Detail from "../pages/Detail";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Sidebar/> //전체 페이지 내 사이드 바 렌더링
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
