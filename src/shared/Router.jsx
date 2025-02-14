5bd419cbf3665bd404ae76e78af3fde5103b8981
JDY <wjsehdud@nate.com>
2025-02-14 18:04:47 +0900

    테스트종료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+
 
 const Router = () => {
   return (
