3c811cc14ebf89bf00dd096f1ffdb5ee009e7947
nahyun park <pnh13584@gmail.com>
2025-02-16 00:09:09 +0900

    Add: fileinputs 컴포넌트 추가

---
+++ src/components/inputs/Fileinputs.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const Fileinputs = () => {
+  return (
+    <>
+      <div className="filebox">
+        <StdFileInput className="upload-name" placeholder="첨부파일" />
+        <StdLabel htmlFor="file">파일찾기</StdLabel>
+        <input type="file" id="file" />
+      </div>
+    </>
+  );
+};
+
+export default Fileinputs;
+
+const StdFileInput = styled.input`
+  display: inline-block;
+  width: 300px;
+  height: 300px;
+  padding: 0 10px;
+  vertical-align: middle;
+  border: 1px solid #dddddd;
+  color: #999999;
+`;
+
+const StdLabel = styled.label`
+  display: inline-block;
+  padding: 10px 20px;
+  color: #fff;
+  vertical-align: middle;
+  background-color: #999999;
+  cursor: pointer;
+  height: 40px;
+  margin-left: 10px;
+`;
--- src/pages/AddFeedPage.jsx
+++ src/pages/AddFeedPage.jsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import CreateFeedInputs from '../components/inputs/CreateFeedInputs';
+import Fileinputs from '../components/inputs/Fileinputs';
+
 const AddFeedPage = () => {
-  return <div>피드추가페이지입니다</div>;
+  return (
+    <>
+      <div>피드추가페이지입니다</div>
+      <Fileinputs />
+      <div>
+        <CreateFeedInputs />
+      </div>
+    </>
+  );
 };
 
 export default AddFeedPage;
