9da4be8404ae0abb90c64e5987bcc54979e4735b
hj <hyeonjubaek06@gmail.com>
2025-02-16 06:09:36 +0900

    Feat: SearchBarInput 컴포넌트에 검색어 필터링 기능 추가

--- src/components/inputs/SearchBarInput.jsx
+++ src/components/inputs/SearchBarInput.jsx
@@ -1,21 +1,48 @@
 
 const SearchBarInput = () => {
     const [search, setSearch] = useState('');
+    const [searchFeedData, setSearchFeedData] = useState([]);
 
+    // 검색어 변경 핸들러
     const handleSearchChange = (e) => {
         setSearch(e.target.value);
     };
 
+    // 검색어에 따라 필터링된 데이터 생성
+    const filterSearch = searchFeedData.filter((searchInfo) =>
+        searchInfo.name.toLowerCase().includes(search.action())
+    );
+
+    // 가짜 데이터 가져오기 (API 요청 대신)
+    useEffect(() => {
+        // 예제 데이터
+        setSearchFeedData([
+            { id: 1, name: "옥씨부인전" },
+            { id: 2, name: "말할 수 없는 비밀" },
+            { id: 3, name: "검은수녀들" },
+        ]);
+    }, []);
+
     return (
         <div>
+            <ul>
+                {filterSearch.map((feed) => (
+                        <li className="feed" key={feed.id}>
+                            <img
+                                src="${feed.id}"
+                                alt={"feed.name"}
+                            />
+                            <p>{feed.name}</p>
+                        </li>
+                    ))}
+            </ul>
             <form action="">
                 <input
                     type="text"
-                    name="search"
+                    name="searchInput"
                     value={search}
                     onChange={handleSearchChange}
                     placeholder="검색어를 입력하세요"
-
                 />
             </form>
         </div>
