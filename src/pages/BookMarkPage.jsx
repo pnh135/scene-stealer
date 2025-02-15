b2e20239a75e58f8a3d5a287f1bacaa04a32b638
JungminLee97 <jungmin7618@naver.com>
2025-02-15 12:55:22 +0900

    Stlye: 북마크된카드 스타일입히기완료

--- src/pages/BookMarkPage.jsx
+++ src/pages/BookMarkPage.jsx
@@ -1,47 +1,73 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import MOCK_DATA from '../data/MOCK_DATA';
+import styled from 'styled-components';
+import { Heart, MessageCircle, Star } from 'lucide-react';
 
 const BookMarkPage = () => {
   const [bookmarks, setBookmarks] = useState([]);
 
   useEffect(() => {
-    const savedBookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    const savedBookmarks = JSON.parse(localStorage.getItem('bookmark')) || [];
     setBookmarks(savedBookmarks);
   }, []);
 
   const RemoveBookMark = (id) => {
     const updatedBookmarks = bookmarks.filter((bo) => bo !== id);
     setBookmarks(updatedBookmarks);
-    localStorage.setItem('bookmarks', JSON.stringify(updatedBookmarks));
+    localStorage.setItem('bookmark', JSON.stringify(updatedBookmarks));
   };
 
   return (
     <div>
       북마크페이지입니다
-      {bookmarks.length > 0 ? (
-        bookmarks.map((id) => {
-          const item = MOCK_DATA.find((li) => li.id === id);
-          return item ? (
-            <div key={id}>
-              <img src={item.img_url} alt={item.korean_name} />
-              <div>{item.korean_name}</div>
-              <div>{item.description}</div>
-              <button
-                onClick={() => {
-                  RemoveBookMark(item.id);
-                }}
-              >
-                북마크삭제
-              </button>
-            </div>
-          ) : null;
-        })
-      ) : (
-        <p>북마크된 아이템이 없습니다.</p>
-      )}
+      <FeedListWrapper>
+        {bookmarks.length > 0 ? (
+          bookmarks.map((id) => {
+            const item = MOCK_DATA.find((li) => li.id === id);
+            return item ? (
+              <FeedListContent key={id}>
+                <FeedListContentTitle>{item.korean_name}</FeedListContentTitle>
+                <img src={item.img_url} alt={item.korean_name} />
+                <FeedListIcon>
+                  <Heart />
+                  <MessageCircle />
+                  <Star
+                    onClick={() => {
+                      RemoveBookMark(item.id);
+                    }}
+                  />
+                </FeedListIcon>
+              </FeedListContent>
+            ) : null;
+          })
+        ) : (
+          <p>북마크를 추가해주세요!</p>
+        )}
+      </FeedListWrapper>
     </div>
   );
 };
+const FeedListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 1rem;
+`;
 
+const FeedListContent = styled.div`
+  list-style: none;
+  width: 12rem;
+  border-radius: 1rem;
+  background-color: green;
+  justify-items: center;
+`;
+
+const FeedListContentTitle = styled.p`
+  margin: 0.5rem 0;
+  padding-left: 0.5rem;
+`;
+
+const FeedListIcon = styled.div`
+  padding-left: 0.5rem;
+`;
 export default BookMarkPage;
