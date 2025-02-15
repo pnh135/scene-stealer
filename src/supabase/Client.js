b831aaaf0f6594c80c6b366aa0fcea195b3fa7f4
123456466 <sunji7378@gmail.com>
2025-02-15 20:23:25 +0900

    Feat : 수파베이스 세팅

--- src/supabase/Client.js
+++ src/supabase/Client.js
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(import.meta.env.VITE_APP_SUPABASE_URL, import.meta.env.VITE_APP_SUPABASE_ANON_KEY);
+//env가 안돼서 월요일에 튜터님에게 갈 예정정
+const supabase = createClient(
+    // import.meta.env.VITE_APP_SUPABASE_URL, 
+    // import.meta.env.VITE_APP_SUPABASE_ANON_KEY
+    "https://fvoinfbnjitrkplgaody.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2b2luZmJuaml0cmtwbGdhb2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MzQwOTYsImV4cCI6MjA1NTAxMDA5Nn0.NPwbLjNX9rlN0oFpD-nBlccJL6mnwd_pHaKIt_O2drk"
+);
 
 export default supabase;
