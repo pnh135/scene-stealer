074391aeb3a43470c9c47070f0c38f8558a279d8
123456466 <sunji7378@gmail.com>
2025-02-16 13:26:42 +0900

    Feat : 회원가입 조건 추가[비밀번호 길이, 아이디 조건]

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,31 +1,59 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import supabase from '../supabase/Client';
 
 const SignUpPage = () => {
-  const [id, setId] = useState('');
+  const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [name, setname] = useState('');
   const [email, setEmail] = useState('');
 
   const handleSignUp = async (e) => {
     e.preventDefault();
-//값을 수파베이스에 넘기는 함수
-const {data,error} = await supabase.auth.signUp({
-  email,
-  password,
-  options: {
-    data: {
-      user_id: id,
-      password,
-      name
+
+    if (password.length < 6) {
+      return alert('비밀번호는 6글자 이상 이여야 합니다.');
     }
-  }
-})
+
+    const textComparison = /^[a-z|A-Z|0-9]+$/;
+
+    if (!textComparison.test(userId)) {
+      return alert('아이디에는 영어와 숫자만 들어가야 합니다.');
+    }
+
+    const getUserId = async () => {
+      const { data, error } = await supabase.from('user_info').select('user_id');
+      console.log(data);
+    };
+    getUserId();
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          user_id: userId,
+          name,
+          email
+        }
+      }
+    });
+    if (error) throw error;
+
+    setUserId('');
+    setPassword('');
+    setEmail('');
+    setname('');
   };
 
   return (
     <form onSubmit={handleSignUp}>
-      <input placeholder="아이디를 입력해 주세요." type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <input
+        placeholder="아이디를 입력해 주세요."
+        type="text"
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+      />
       <input
         placeholder="비밀번호를 입력해 주세요."
         type="password"
