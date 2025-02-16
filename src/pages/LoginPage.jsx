55e6cc48dc1674c47965dc3c35af3c52beb08af3
123456466 <sunji7378@gmail.com>
2025-02-16 21:08:01 +0900

    Feat : 로그인 기능 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import supabase from '../supabase/Client';
 
 const LoginPage = () => {
-  //연결 확인용 함수
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
+
   const login = async () => {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password
-    });
+    e.preventDefault();
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+      });
+
+      if (error) throw error;
+
+      alert('로그인 성공!');
+    } catch (error) {
+      alert(error.message);
+      console.error('로그인 오류:', error);
+    }
   };
 
   return (
-    <form>
-      <input placeholder="email"></input>
-      <input placeholder="PW"></input>
+    <form onSubmit={login}>
+      <input placeholder="email" type="email" value={email} onChange={(e) => setemail(e.target.value)} required />
+      <input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setpassword(e.target.value)}
+        required
+      />
       <button>로그인</button>
     </form>
   );
