8853ef4b7b2a35ec0d489a1b0b0ef7a6e893add0
hj <hyeonjubaek06@gmail.com>
2025-02-17 16:40:00 +0900

    refactor: CreateFeedInputs에서 tagsArray 상태 제거

--- src/components/inputs/CreateFeedInputs.jsx
+++ src/components/inputs/CreateFeedInputs.jsx
@@ -5,7 +5,6 @@ const CreateFeedInputs = () => {
     const [createFeedInput, setCreateFeedInput] = useState({
         feedTitle: "",
         contentTitle: "",
-        tagsArray: [],
         tagInput: "",
     });
 
