6d6689fb0aef8042373b4250e532478dcf2e46b5
kwakhana <you@example.com>
2025-02-17 19:15:48 +0900

    Add: 프로필 이미지 변경 기능 추가

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
-import MainHeader from '../components/MainHeader'
-import { MainPageAside, MainPageContent, MainPageMain } from './MainPage'
-import SideBar from '../components/SideBar'
-import MainFooter from '../components/MainFooter'
-import ActionButton from '../components/ActionButton'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+const SettingPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;    
+`;
+
+const SettingPageImage = styled.img`
+  width: 200px;        
+  height: 200px;        
+  border-radius: 50%;   
+  object-fit: cover;
+  border: 2px solid #ccc; 
+`;
+
+const SettingPageInput = styled.input`
+  margin-top: 20px;    
+`
+const SettingPageButton = styled.button`
+  width: 200px;
+  text-align: center;
+  margin-top: 20px;    
+  padding: 10px 20px;  
+  background-color: black;
+  color: white;        
+  border-radius: 5px; 
+`
 const SettingPage = () => {
+  const [src, setSrc] = useState('')
+
+  //선택된 파일을 이미지 주소로 보내주는 로직
+  const handleInput = (e) => {
+    const file = e.target.files[0];
+    console.log(file)
+    if (file) {
+      const reader = new FileReader(); 
+      console.log(reader)
+      reader.onloadend = () => {
+        setSrc(reader.result); 
+      }
+      reader.readAsDataURL(file); 
+      }
+  };
   return (
-    <div>
-        <MainHeader />
-        <MainPageContent>
-          <MainPageAside>
-            <SideBar />
-          </MainPageAside>
-          <MainPageMain>
-            <ActionButton/>
-            <p>프로필 설정</p>
-          </MainPageMain>
-        </MainPageContent>
-        <MainFooter />
-    </div>
-  )
+    <SettingPageWrapper>
+      <SettingPageImage src={src} alt=""/>
+      <SettingPageInput type="file" onChange={handleInput}/>
+      <SettingPageButton>수정하기</SettingPageButton>
+    </SettingPageWrapper>
+  );
 }
 
-export default SettingPage
+export default SettingPage;
