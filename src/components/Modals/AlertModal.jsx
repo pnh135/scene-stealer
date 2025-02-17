7f852955e73216c935cf2cdf67e29b884043477c
Koi-0 <dndud5182@gmail.com>
2025-02-18 03:54:56 +0900

    Merge branch 'public-setting' of https://github.com/pnh135/scene-stealer into public-home

--- src/components/Modals/AlertModal.jsx
+++ src/components/Modals/AlertModal.jsx
@@ -3,7 +3,7 @@ import DefaultModal from '../Modals/DefaultModal';
 const AlertModal = ({ isOpen, onClose }) => {
   return (
     <DefaultModal isOpen={isOpen} onClose={onClose}>
-      알림
+      <p>알림</p>
     </DefaultModal>
   );
 };
